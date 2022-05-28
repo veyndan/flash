898ea48bf9476a4d332eee91d175f9657668a307
Veyndan Stuart <veyndan@gmail.com>
2022-05-28 22:39:32 +0200

    Fix Generate button styling by upgrading to Svelte

---
+++ ts/src/Generate.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    const {
+        LabelButton,
+        WithState,
+        //@ts-ignore
+    } = components;
+
+    const key = "generate";
+</script>
+
+<WithState
+    {key}
+    update={() => document.queryCommandState(key)}
+    let:updateState
+>
+    <LabelButton
+        tooltip="Generate Note"
+        on:click={(event) => {
+            pycmd("myankiplugin:generate")
+            document.execCommand(key);
+            updateState(event);
+        }}
+    >
+        Generate
+    </LabelButton>
+</WithState>
